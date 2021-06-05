eaced37a73250ba1bfa20ec333b6eef9d9a610eb
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2021-06-05 09:31:12 +0300

    feat: implenetation log in & log out

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+
+        const userCollection = client.db().collection('users');
+        const user = await userCollection.findOne({ email: credentials.email });
+
+        if (!user) {
+          client.close();
+          throw new Error('No user found!');
+        }
+
+        const isValidPassword = await verifyPassword(credentials.password, user.password);
+
+        if (!isValidPassword) {
+          client.close();
+          throw new Error('Could not log you in!');
+        }
+
+        client.close();
+        return { email: user.email };
+      },
+    }),
+  ],
+});
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,33 +2,44 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 const handler = async (req, res) => {
-  const data = req.body;
-  const { email, password } = data;
+  if (req.method === 'POST') {
+    const data = req.body;
+    const { email, password } = data;
 
-  if (
-    !email ||
-    !email.includes('@') ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res
-      .status(422)
-      .json({
-        message:
-          'Invalid input - password should be at least 7 characters long.',
-      });
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res
+        .status(422)
+        .json({
+          message:
+            'Invalid input - password should be at least 7 characters long.',
+        });
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+    const existingUser = await db.collection('users').findOne({email});
+
+    if (existingUser) {
+      res.status(422).json({ message: 'User exist already!' });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'Created user!' });
+    client.close();
   }
-
-  const client = await connectToDatabase();
-  const db = client.db();
-  const hashedPassword = hashPassword(password);
-
-  const result = await db.collection('users').insertOne({
-    email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: 'Created user!' });
 };
 
 export default handler;
