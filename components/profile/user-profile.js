afb90adc928a068a47f962bfd002c20a2ffcb99e
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2021-06-05 10:44:03 +0300

    feat: implemented change password

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -5,23 +5,37 @@ function UserProfile() {
   // const [isLoading, setIsLoading] = useState(true);
 
   // useEffect(() => {
-  //   getSession().then(session => {
+  //   getSession().then((session) => {
   //     if (!session) {
   //       window.location.href = '/auth';
   //     } else {
   //       setIsLoading(false);
   //     }
   //   });
-  // }, [])
+  // }, []);
 
   // if (isLoading) {
-  //   return <p className={classes.profile}>Loading...</p>
+  //   return <p className={classes.profile}>Loading...</p>;
   // }
+
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
